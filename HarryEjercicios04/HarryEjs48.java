06224caf792562bb39bb2de741d094c27d986f22
Jhackns <jhackmaniaz64@gmail.com>
2022-05-27 01:57:14 +0000

    HarryEjercicios04

---
+++ HarryEjercicios04/HarryEjs48.java
@@ -0,0 +1,24 @@
+package HarryEjercicios04;
+
+import java.util.Scanner;
+
+public class HarryEjs48 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i;
+        double deposito, suma;
+        suma = 0;
+        for (i=1; i<=10; i++) {
+            System.out.print("PROCESO " + i);
+            System.out.print("Ingresa el valor de deposito: ");
+            deposito = in.nextDouble();
+            in.nextLine();
+            suma=suma*1.08+deposito;
+            System.out.println();
+        }
+        System.out.println("Valor de suma: " + suma);
+    }
+
+}
+
